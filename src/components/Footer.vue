a9f845055ea8b23ba78982d9262175653718fb8a
pjbatz16 <78991855+pjbatz16@users.noreply.github.com>
2026-03-05 15:38:49 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,1 @@
-<template>
-	<footer class="bg-dark text-light py-2">
-	   <div class="container text-center">
-	     <p class="mb-1 fw-bold">Philip John Batulan</p>
-	     <p class="mb-1">Full Stack Web Developer</p>
-	     <p class="mb-0">© 2025 All rights reserved.</p>
-	   </div>
-	 </footer>
-</template>
+
